099f37d0bfae60e32178a311ea580f84c059bd3e
Karl Xu <karl.yogi.xu@gmail.com>
2018-05-20 12:41:01 +0800

    little go: update array

--- the-little-go-book/slice.go
+++ the-little-go-book/slice.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-    scores := [5]int{1,4,293,4,9}
+    scores := []int{1,4,293,4,9}
     for index, element := range scores {
         fmt.Printf("The %d indexed number's value is %d \n", index, element)
     }
