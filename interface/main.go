a2b3f74e5ba029c8cbad57cb86204eb43666f27a
Karl Xu <karl.yogi.xu@gmail.com>
2020-07-21 20:46:46 +0800

    add interface example

---
+++ interface/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+/**
+- define interface Shape including area() and diameter()
+- define type rectangle and circle implement interface Shape
+**/
+
+type Shape interface {
+	area() float64
+	perimeter() float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (c Circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c Circle) perimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func main() {
+	circle := Circle{4}
+	fmt.Println("Circle radius:", circle.radius)
+	fmt.Println("Circle diameter:", circle.perimeter())
+	fmt.Println("Circle area:", circle.area())
+
+}
